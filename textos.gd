c204d258985d3b07397d809fc334e368b198869e
Leone242 <leone.tg@outlook.com>
2024-03-09 19:30:05 -0300

    textos ainda

--- textos.gd
+++ textos.gd
@@ -20,15 +20,30 @@ var troll = {
 var caramunhao = {
 	"demonio_a" = "Demonio: Um gnomo! \nFaz tempo que não tenho carne magra.\n",
 	"demonio_b" = "Demonio: Você acha que uma pedra vai fazer alguma diferença? \nVOCÊ ACHA ERRADO, PETISCO.\n",
-	"demonio_c" = "Demonio: CHURRASQUINHO DE GNOMO POR CONTA DA CASA, \nCOM CERVEJA DE ANÃO\n",
+	"demonio_c" = "Demonio: CHURRASQUINHO DE GNOMO POR CONTA DA CASA, \nCOM CERVEJA DE ANÃO!\n",
 	"acao_a" = "Você olha para a pancinha se formando no demônio e comenta \n'tá de quantos meses?'\n",
-	"demonio_d" = "Demonio: ...CALA A BOCA SEU PEDAÇO DE CARNE, \nTÁ VENDO ESSES BRAÇOS? \n...se bem que você não enxerga até essa altura, já que é o anão do anão hehe\n",
+	"demonio_d" = "Demonio: ...CALA A BOCA SEU PEDAÇO DE CARNE, \nTÁ VENDO ESSES BRAÇOS? \n...se bem que você não enxerga até essa altura, já que é o anão do anão hehe.\n",
 	"troll" = "Troll: ...POOOOW! O ANÃO²\n",
 	"demonio_e" = "Demonio: GRRR tá com pouca criatividade seu MERDA, \nEU SEI COMO ESTÁ, \nEU OLHO NO ESPELHO!\n",
-	"acao_b" = " Você olha pras pernas finas do demônio \n...'tá um picolé filezinho hein, \nsó as pernas de saracura essas coxinhas'\n",
-	"demonio_f" = "Demonio: GRRRR CALA A BOCA E PARA DE FALAR DO MEU FÍSICO!\n"
+	"acao_b" = " Você olha pras pernas finas do demônio \n...'tá um picolé filezinho hein, \nsó as pernas de saracura essas coxinhas.'\n",
+	"demonio_f" = "Demonio: GRRRR CALA A BOCA E PARA DE FALAR DO MEU FÍSICO!\n",
+	"gnomo" = "Porque carne gorda você já vê todo dia \nassim que acorda.\n"
 	
-	
+}
+
+var efeitos = {
+	"raiva" = "O demônio já está com tanta raiva \nque não ouve você zoando ele.\n",
+	"dano_raiva" = "50 de dano. \nO Caramunhão se feriu ao errar um golpe, \nsocando a parede.\n",
+	"raiva_confuso" = "Mesmo enraivecido, ele tenta compreender o que você fala.",
+	"dano_confuso" = "80 de dano. \nO demônio tenta dar um cruzado de direita, \nmas está tão confuso que acaba acertando a própria cara.\n",
+	"trocadilho" = "Você faz um trocadilho sobre cascos de bode.",
+	"resposta_demonio" = "Demonio: Sério que você tá falando disso? \nJá bastou os Druidas estranhos na semana passada.\n",
+	"chifre" = "Você comenta sobre o formato dos chifres \ne fala sobre como são muuuuuuuuuuuuuuito bonitos.\n",
+	"força" = "-1 de força.\n",
+	"desinteresse" = "O demônio não parece interessado \nem saber de piadas sem graça por enquanto.\n",
+	"vitória" = "Vitória por trocadilho ruim.\n",
+	"fim" = "Demonio: Quer saber? \nPega essa merda e sai daqui, \nperdi o apetite depois dessas.\n",
+	"chave" = "O deônio deixa cair uma chave.\n"
 }
 
 
